7e2f2ec386606a06daf821394ba109fdf4df8055
Sean Bocz <seanboczulak@gmail.com>
2016-03-26 16:45:03 -0600

    Initial commit

---
+++ Efinity/Database.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efinity
+{
+	/// <summary>
+	/// Fake database for demo purposes
+	/// </summary>
+	class Database
+	{
+		public DataTable Version;
+		public DataTable Application;
+		
+		public Database()
+		{
+			MakeApplicationTable();
+			MakeVersionTable();
+		}
+
+		private void MakeApplicationTable()
+		{
+			Application = new DataTable();
+			Application.Columns.Add("ApplicationName");
+			Application.Columns.Add("HashKey");
+		}
+
+		private void MakeVersionTable()
+		{
+			Version = new DataTable();
+			Version.Columns.Add("VersionNumber");
+			Version.Columns.Add("ApplicationName");
+			Version.Columns.Add("HashKey");
+		}
+	}
+}
---
+++ Efinity/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Efinity
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Database db = new Database();
+			
+		}
+	}
+}
